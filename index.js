b6755784a127a1d815b136a3ecbd5341470a23b3
FreitagDavid <freitagdavid@outlook.com>
2019-01-15 16:04:37 -0500

    index: Get zoo by id working

--- index.js
+++ index.js
@@ -29,6 +29,19 @@ server.post('/api/zoos', (req, res) => {
   })
 })
 
+server.get('/api/zoos/:id', (req, res) => {
+  const {id} = req.params
+  db('zoos').where('id', 'like', id).then(result => {
+    if (result.length === 0) {
+      res.status(404).json({"errMessage": "The zoo by this id does not exist."})
+    } else {
+      res.status(200).json(result)
+    }
+  }).catch(err => {
+    res.status(500).json(err)
+  })
+})
+
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
