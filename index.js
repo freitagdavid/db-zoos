ca1aa50f808b027b27c6129c8574ef7f701a4bf1
FreitagDavid <freitagdavid@outlook.com>
2019-01-15 15:40:13 -0500

    index: Can request all zoos

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const helmet = require('helmet');
-
+const knex = require('knex');
+const dbConfig = require('./knexfile')
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
+const db = knex({...dbConfig.development})
+
 // endpoints here
+
+server.get('/api/zoos', (req, res) => {
+  db('zoos').then(zoos => {
+    res.status(200).json({zoos});
+  }).catch(err => {
+    res.status(500).json(err);
+  })
+})
 
 const port = 3300;
 server.listen(port, function() {
