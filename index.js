3cd4b4b636e8e4d0c6d141fdbcdcd65572bb13ee
FreitagDavid <freitagdavid@outlook.com>
2019-01-15 16:18:04 -0500

    index: Delete implemented

--- index.js
+++ index.js
@@ -1,48 +1,77 @@
 const express = require('express');
 const helmet = require('helmet');
 const knex = require('knex');
-const dbConfig = require('./knexfile')
+const dbConfig = require('./knexfile');
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
-const db = knex({...dbConfig.development})
+const db = knex({ ...dbConfig.development });
 
 // endpoints here
 
 server.get('/api/zoos', (req, res) => {
-  db('zoos').then(zoos => {
-    res.status(200).json({zoos});
-  }).catch(err => {
-    res.status(500).json(err);
-  })
-})
+    db('zoos')
+        .then(zoos => {
+            res.status(200).json({ zoos });
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
 
 server.post('/api/zoos', (req, res) => {
-  db('zoos')
-  .insert(req.body)
-  .then(result => {
-    res.status(201).json({result})
-  }).catch(err => {
-    res.status(500).json(err)
-  })
-})
+    db('zoos')
+        .insert(req.body)
+        .then(result => {
+            res.status(201).json({ result });
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
 
 server.get('/api/zoos/:id', (req, res) => {
-  const {id} = req.params
-  db('zoos').where('id', 'like', id).then(result => {
-    if (result.length === 0) {
-      res.status(404).json({"errMessage": "The zoo by this id does not exist."})
-    } else {
-      res.status(200).json(result)
-    }
-  }).catch(err => {
-    res.status(500).json(err)
-  })
-})
+    const { id } = req.params;
+    db('zoos')
+        .where('id', 'like', id)
+        .then(result => {
+            if (result.length === 0) {
+                res.status(404).json({
+                    errMessage: 'A zoo by this id does not exist.',
+                });
+            } else {
+                res.status(200).json(result);
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
+
+server.delete('/api/zoos/:id', (req, res) => {
+    const { id } = req.params;
+    db('zoos')
+        .where('id', 'like', id)
+        .del()
+        .then(result => {
+            if (result === 1) {
+                res.status(200).json({
+                    success: `Successfully deleted zoo ${id}`,
+                });
+            } else {
+                res.status(404).json({
+                    errMessage: 'A zoo by this id does not exist',
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
 
 const port = 3300;
 server.listen(port, function() {
-  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+    console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
